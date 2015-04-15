933e3249204c3df9cbd7a50a7d176ba5d984cf36
Jamison Gray <jamison.m.gray@vanderbilt.edu>
2015-04-15 12:54:16 -0500

    Removed unused variables

--- src/CPUStat.h
+++ src/CPUStat.h
@@ -13,12 +13,6 @@ public:
    std::string read(); 
 
 private:
-    //information about cpu is taken from /proc/cpuinfo
-   std::string m_vendor;
-   std::string m_model_name;
-   double m_freq;
-   int m_procesors;
-
     ///proc/stat
     double m_nice;
     double m_user;
